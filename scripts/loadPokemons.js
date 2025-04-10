0169ecb36867ba01811afcded10f1e3f9ec9b60d
Raul Ciucalau <ciucalauraul@gmail.com>
2025-04-10 21:43:05 +0200

    code cleanup

--- scripts/loadPokemons.js
+++ scripts/loadPokemons.js
@@ -2,23 +2,25 @@ let pokemons = [];
 
 async function onLoadFunc() {
     let response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-    let responseAsJson = await response.json();
+    let pokemonData = await response.json();
     let contentRef = document.getElementById('content');
-    for (let index = 0; index < responseAsJson.results.length; index++) {
-        let pokemon = responseAsJson.results[index];
-        let pokemonData = await fetch(pokemon.url);
-        let pokemonDetails = await pokemonData.json();
-        let imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`;
-        let types = pokemonDetails.types.map(type => type.type.name);
-        pokemons.push({
-            name: pokemon.name,
-            imageUrl: imageUrl,
-            types: types
-        });
+    for (let i = 0; i < pokemonData.results.length; i++) {
+        await fetchPokemon(pokemonData.results[i], i);
     }
     contentRef.innerHTML = '';
     Object.keys(pokemons).forEach(key => {
-        let pokemon = pokemons[key];
-        contentRef.innerHTML += getContentTemplate(pokemon);
+        contentRef.innerHTML += getContentTemplate(pokemons[key]);
     });
+}
+
+async function fetchPokemon(pokemon, index) {
+    let res = await fetch(pokemon.url);
+    let details = await res.json();
+    let imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`;
+    let types = details.types.map(t => t.type.name);
+    pokemons.push({
+         name: pokemon.name, 
+         imageUrl, 
+         types 
+        });
 }
